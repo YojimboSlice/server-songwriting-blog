318d1049c5dd2f566f28432f75b20a1685188c7c
YojimboSlice <jamesryanlan@gmail.com>
2023-05-11 22:36:48 -0700

    Fix client.close

--- netlify/functions/get-data.js
+++ netlify/functions/get-data.js
@@ -1,34 +1,31 @@
-const { MongoClient } = require('mongodb');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
 const handler = async (event, context) => {
+  let client;
   try {
-    const uri = process.env.MONGO_URI; // Replace with your MongoDB URI
-    const client = new MongoClient(uri, {
+    const uri = process.env.MONGO_URI;
+    client = new MongoClient(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     await client.connect();
     const collection = client
       .db('songwriting_blog')
-      .collection('song_writing_blog_posts'); // Replace with your collection name
+      .collection('song_writing_blog_posts');
     const cursor = collection.find();
     const data = await cursor.toArray();
     console.log(data, typeof data);
-    await client.close();
     return {
       statusCode: 200,
       body: JSON.stringify(data),
     };
   } catch (error) {
-    console.error(error); // Log the error to the console
+    console.error(error);
     return {
       statusCode: 500,
       body: JSON.stringify({ message: 'Internal server error' }),
     };
+  } finally {
+    if (client) {
+      await client.close();
+    }
   }
 };
-
-module.exports = { handler };
